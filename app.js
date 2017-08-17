690dbcbfc0997ed0eaf99b3815436b14e9c013ec
Kevin Harrison <kharrison7@elon.edu>
2017-08-17 13:49:30 -0400

    This adds a few changes to todo file

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 //app.js code here.
-var express = require('express');
-var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
+let express = require('express');
+let bodyParser = require('body-parser');
+let expressValidator = require('express-validator');
 const mustacheExpress = require('mustache-express');
 const data = require('./items.js');
-
-
 // Create app
-var app = express();
-
+let app = express();
 // Set app to use bodyParser()` middleware.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,35 +21,31 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   // Set 'action' to '/'
-  res.render('users', data)
-  // var html = '<form action="/" method="post">' +
-  //            '<h1>User Name</h1>' +
-  //            '<p>Enter a username</p>' +
-  //            '<input type="text" name="user" placeholder="user name" />' +
-  //            '<button type="submit">Submit</button>' +
-  //       '</form>';
-  // res.send(html);
-});
+  res.render('todo', data)});
 
 // Receives data from form (action='/')
 // 'req.body' now contains form data.
 app.post('/', function(req, res){
+  console.log("req sent");
   //Call req.checkBody function.
     //Pass inputs to validate.
     //Tell middleware which validators to apply (chain one or more).
-    req.checkBody("user", "You must enter a username!").notEmpty();
+    // req.checkBody("user", "You must enter a username!").notEmpty();
 
-    var errors = req.validationErrors();
-    if (errors) {
-      // Render validation error messages
-      var html = errors;
-      res.send(html);
-    } else {
-      console.log("User: " + req.body.user);
+    console.log("User 1: " + req.body.user);
+
+    // Render validation error messages
+    // var errors = req.getValidationResult();
+    // if (errors) {
+    //   var html = errors;
+    //   res.send(html);
+    // } else {
+      console.log("Body: " + req.body);
       var user = req.body.user;
+      console.log("User 2: " + user);
       var html = '<p>Your user name is: </p>' + user;
       res.send(html);
-    }
+    // }
   });
 
 app.listen(3000, function(){

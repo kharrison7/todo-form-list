1e0898ff65f7f70f6991b0a3f43aa221c38b36d5
Kevin Harrison <kharrison7@elon.edu>
2017-08-17 20:12:24 -0400

    This adds a working done section.

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ let bodyParser = require('body-parser');
 let expressValidator = require('express-validator');
 const mustacheExpress = require('mustache-express');
 const data = require('./items.js');
+
+const file = './fill.json';
+const fs = require('fs');
+
 // Create app
 let app = express();
 // Set app to use bodyParser()` middleware.
@@ -20,13 +24,30 @@ app.set('views', './views');
 app.set('view engine', 'mustache');
 app.use(express.static(__dirname + '/public'));
 
-let todosList = [];
+// let todosList = [];
 // {entry: "Example Todo", done: "false"}
 app.get('/', function(req, res){
   // Set 'action' to '/'
   console.log("Basic get run!");
   // console.log("Last item: "+todosList[todosList.length-1].entry);
-  res.render('todo', {todo_list: todosList})});
+  // res.render('todo', {todo_list: todosList})});
+
+  //fs.readFile reads the fill.json file
+  fs.readFile('fill.json', 'utf8', function readFileCallback(err, data){
+      if (err){
+          console.log(err);
+      } else {
+      obj = JSON.parse(data);//Turns fill.json into an object named obj
+      var todoarray = obj.todoArray; //declares makes the todoarray
+      var donearray = obj.doneArray; //declares makes the donearray
+      // this makes todosMustache the todoarray var
+      res.render('todo', { todosMustache: todoarray,  doneMustache: donearray});
+  }});
+  // This will make todosMustache the todosArray
+  // res.render('todo', { todosMustache: todosArray });
+});
+
+
 
 
 
@@ -39,6 +60,17 @@ app.post('/', function(req, res){
     //Tell middleware which validators to apply (chain one or more).
     // req.checkBody("user", "You must enter a username!").notEmpty();
 
+    var addtolist = req.body.user; //Gets the text in the input tag with name ="user"
+    fs.readFile('fill.json', 'utf8', function readFileCallback(err, data){
+        if (err){
+            console.log(err);
+        } else {
+        obj = JSON.parse(data); //now its an object
+        obj.todoArray.push(addtolist); //pushes the text to an array
+        json = JSON.stringify(obj); //converts back to json
+        fs.writeFile('fill.json', json, 'utf8'); // writes to file
+    }});
+    res.redirect('/');//reloads page
 
     // Render validation error messages
     // var errors = req.validationErrors();
@@ -47,14 +79,38 @@ app.post('/', function(req, res){
     //   var html = errors;
     //   res.send(html);
     // } else {
-      let user = req.body.user;
+      // let user = req.body.user;
     //   console.log("Entry: " + user);
       // var html = '<p>Your user name is: </p>' + user;
       // res.send(html);
-      todosList.push({entry: `${user}`, done: "false"});
-      res.redirect('/');
+      // todosList.push({entry: `${user}`, done: "false"});
+      // res.redirect('/');
       // res.render('todo', data);
     // }
+  });
+
+
+  //This is dynamic, meaning any time i click a button that is not "/", this will fire
+  app.post("/:dynamic", function (req, res) {
+    fs.readFile('fill.json', 'utf8', function readFileCallback(err, data){
+        if (err){
+            console.log(err);
+        } else {
+        obj = JSON.parse(data); //now its an object
+          // iterate over each element in the array
+          for (var i = 0; i < obj.todoArray.length; i++){
+          // look for the entry with a matching value
+            if (obj.todoArray[i] === req.params.dynamic){//req.params.dynamic finds the value of dynamic
+              var change = obj.todoArray[i]; //this sets change to the string to delete
+              console.log("I am deleting " + change);//logs the string to delete
+              obj.doneArray.push(change);//pushes the string to delete to the done array
+              obj.todoArray.splice(i, 1);//splices the string from the to do array
+            }
+          }
+        json = JSON.stringify(obj); //converts back to json
+        fs.writeFile('fill.json', json, 'utf8'); // writes to file
+    }});
+    res.redirect('/');//reloads page
   });
 
 
